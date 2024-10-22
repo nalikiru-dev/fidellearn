aea4ba78e97d1ad75666ec3b6063a1aa8d95661b
nalikiru <nalikiru@gmail.com>
2024-10-22 07:23:56 -1200

    Last trial commit to publish is now

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,39 +1,40 @@
-'use client'
-
-import { useUser } from '@clerk/nextjs'
-import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
-import StudentDashboard from '@/components/Dashboard/Student/StudentDashboard'
-import TeacherDashboard from '@/components/Dashboard/Teacher/TeacherDashboard'
-import StaffDashboard from '@/components/Dashboard/Staff/StaffDashboard'
-import ManagerDashboard from '@/components/Dashboard/Manager/ManagerDashboard'
-
-export default function DashboardPage() {
-  const { isSignedIn, isLoaded, user } = useUser()
-  const router = useRouter()
-
-  useEffect(() => {
-    if (isLoaded && !isSignedIn) {
-      router.push('/')
-    }
-  }, [isSignedIn, isLoaded, router])
-
-  if (!isLoaded || !isSignedIn) {
-    return null
-  }
-
-  const role = user?.publicMetadata.role as string
-
-  switch (role) {
-    case 'student':
-      return <StudentDashboard />
-    case 'teacher':
-      return <TeacherDashboard />
-    case 'staff':
-      return <StaffDashboard />
-    case 'manager':
-      return <ManagerDashboard />
-    default:
-      return <div>Invalid role</div>
-  }
-}
+'use client'
+
+import { useUser } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+import StudentDashboard from '@/components/Dashboard/Student/StudentDashboard'
+import TeacherDashboard from '@/components/Dashboard/Teacher/TeacherDashboard'
+import StaffDashboard from '@/components/Dashboard/Staff/StaffDashboard'
+import ManagerDashboard from '@/components/Dashboard/Manager/ManagerDashboard'
+
+export default function DashboardPage() {
+  const { isSignedIn, isLoaded, user } = useUser()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push('/')
+    }
+  }, [isSignedIn, isLoaded, router])
+
+  if (!isLoaded || !isSignedIn) {
+    return null
+  }
+
+  const role = user?.publicMetadata.role as string
+
+  switch (role) {
+    case 'student':
+      return <StudentDashboard />
+    case 'teacher':
+      return <TeacherDashboard />
+    case 'staff':
+      return <StaffDashboard />
+    case 'manager':
+      return <ManagerDashboard />
+    default:
+      return <div>Invalid role</div>
+  }
+}
+
